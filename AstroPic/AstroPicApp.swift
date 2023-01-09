dd1e085f9d3903f9ecbcf93962490528f79c130f
2007riot <2007riot@gmail.com>
2023-01-09 16:05:44 +0100

    Initial Commit

---
+++ AstroPic/AstroPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPicApp.swift
+//  AstroPic
+//
+//  Created by Galina Aleksandrova on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AstroPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
